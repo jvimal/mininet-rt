d7facbdb0eadca739bed59f4f77778913c343d03
jvimal <jvimal@hs03.scs.stanford.edu>
2010-07-21 11:17:55 -0700

    settings module introduced
    keep things at one place.

---
+++ settings.py
@@ -0,0 +1,13 @@
+"""
+  Settings across modules in a central location.
+  Initialised to default values.
+"""
+
+
+n = 10
+t = 10
+rate = '100mbit'
+outputfile = 'out.html'
+detach = False
+dryrun = False
+
