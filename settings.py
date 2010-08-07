6843f4fba8947cee380bcb187fa0b1f11709d0fa
jvimal <jvimal@hs03.scs.stanford.edu>
2010-08-07 11:30:48 -0700

    moved setting parsing to settings.py

--- settings.py
+++ settings.py
@@ -2,13 +2,21 @@
   Settings across modules in a central location.
   Initialised to default values.
 """
+import getopt
 
+# number of hosts
+n = 3
 
-n = 10
+# time to run tests
 t = 10
-rate = '100mbit'
+# Let rate be none by default..
+rate = None
 outputfile = 'out.html'
+
+# do we just start the hosts and pack?
 detach = False
+
+# just print the commands, useful for debugging
 dryrun = False
 test = ''
 stop = False
@@ -16,7 +24,52 @@ stop = False
 tcpdump = False
 veth = False
 etun = True
+
 cpulimit = None
+cpulimit_switch= None
 # switch container ids will start from 1000+
 # 1001, ...
 offset_switch_id = 1000
+
+
+def set_options(argv):
+  optlist, args = getopt.getopt(argv, 'n:t:r:o:dphT:', [
+    'stop', 'tcpdump', 'dryrun',
+    'test=', 'help', 'cpulimit='
+  ])
+  # prolly a better way of doing this...
+  global n,t,rate,outputfile,detach,dryrun,test,\
+    stop,tcpdump,veth,etun,cpulimit,cpulimit_switch,\
+    offset_switch_id
+  for (o,a) in optlist:
+    if o == '-n':
+      n = int(a)
+    if o == '-t':
+      t = int(a)
+    if o == '-r':
+      rate = a
+      if a.lower() == 'none':
+        rate = None
+    if o == '-o':
+      outputfile = a
+    if o == '-d':
+      detach = True
+    if o == '--dryrun':
+      dryrun = True
+    if o == '-h' or o == '--help':
+      printhelp()
+      sys.exit(0)
+    if o == '--test':
+      test = a
+    if o == '--stop':
+      stop = True
+    if o == '--tcpdump':
+      tcpdump = True
+    if o == '--cpulimit':
+      cpulimit = int(a)
+    if o == '--cpulimit_switch':
+      cpulimit_switch = int(a)
+
+  if outputfile == '':
+    outputfile = 'results-%d-%d.html' % (n, t)
+  
--- test.py
+++ test.py
@@ -13,64 +13,7 @@ import settings as s
 from help import printhelp
 
 def main():
-  # number of hosts
-  s.n = 10
-
-  # time to run, parameter to iperf
-  s.t = 10
-
-  # rate limit interfaces?
-  s.rate = '100mbit'
-
-  # where to store output of experiments
-  s.outputfile = ''
-
-  # should we detach from terminal after booting host?
-  s.detach = False
-
-  # Just check the commands that will be executed
-  s.dryrun = False
-  
-  # Which test to run
-  s.test = ''
-
-  # Run tcpdump on all hosts?
-  s.tcpdump = False
-
-  optlist, args = getopt.getopt(sys.argv[1:], 'n:t:r:o:dphT:', [
-    'stop', 'tcpdump', 'dryrun',
-    'test=', 'help', 'cpulimit='
-  ])
-  for (o,a) in optlist:
-    if o == '-n':
-      s.n = int(a)
-    if o == '-t':
-      s.t = int(a)
-    if o == '-r':
-      s.rate = a
-      if a.lower() == 'none':
-        s.rate = None
-    if o == '-o':
-      s.outputfile = a
-    if o == '-d':
-      s.detach = True
-    if o == '--dryrun':
-      s.dryrun = True
-    if o == '-h' or o == '--help':
-      printhelp()
-      sys.exit(0)
-    if o == '--test':
-      s.test = a
-    if o == '--stop':
-      s.stop = True
-    if o == '--tcpdump':
-      s.tcpdump = True
-    if o == '--cpulimit':
-      s.cpulimit = int(a)
-
-  if s.outputfile == '':
-    s.outputfile = 'results-%d-%d.html' % (s.n, s.t)
-  
+  s.set_options(sys.argv[1:])
   topo = SingleSwitchTopo(s.n)
   m = Mininet(switch=UserSwitch, topo=topo, controller=None, rate=s.rate)
   m.start()
@@ -88,7 +31,7 @@ def main():
 
   if not s.dryrun:
     while True:
-      x = int(raw_input())
+      x = int(raw_input('Without asking why, ype 1 and RET when you want to stop: '))
       if x == 1:
         break
     
@@ -101,6 +44,7 @@ def main():
       ]) + res)
 
   m.stop()
+  # please don't do this unless really needed...
   #m.destroy()
 
 main()
